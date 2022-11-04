3a37bc3f3c6854b29d591dbf66c9e9ac6658371a
Win10Device <unfatalbot@gmail.com>
2022-11-04 17:07:37 +1100

    Update redirect.php

--- redirect.php
+++ redirect.php
@@ -6,8 +6,8 @@ echo "<!DOCTYPE html>";
     $s3   = $_GET['return'];
     
     if ($s2 == "error_js") { ?>
-    <script> window.location.assign(" <?php if($_SERVER['HTTPS']) { echo ("https://"); } else { echo ("http://");} echo ($_SERVER['HTTP_HOST']); echo ("/$s3"); ?> ");</script>
-		<noscript> <meta http-equiv = "refresh" content = "0; url = 
+    	<script> window.location.assign(" <?php if($_SERVER['HTTPS']) { echo ("https://"); } else { echo ("http://");} echo ($_SERVER['HTTP_HOST']); echo ("/$s3"); ?> ");</script>
+	<noscript> <meta http-equiv = "refresh" content = "0; url = 
 		<?php if($_SERVER['HTTPS']) { echo ("https://"); } else { echo ("http://");} echo ($_SERVER['HTTP_HOST']); echo ("/error/?error=nojavascript&return=$s3");?>"></noscript>
 		
 		
